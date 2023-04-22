4668fd0711569285564facfdaa2d27b33c4f6e35
Georgios Kousouris <georgios@yelp.com>
2023-04-22 03:23:27 +0100

    Clean up app.js

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
 import {Pgn} from "./cm-pgn/src/cm-pgn/Pgn.js";
 
-// function findFirstDiscrepancy(repertoire, game) {
-//   let curr_parent = repertoire;
-//   let index = 0;
-//
-//   for (let i =0; i<game.length; i++){
-//
-//     if (curr_parent[index].san == game[i].san) {
-//       index += 1
-//       continue;
-//     }
-//     const sans = curr_parent[index].variations.map(obj => obj[0].san);
-//     if (sans.includes(game[i].san)) {
-//       curr_parent = curr_parent[index].variations[sans.indexOf(game[i].san)]
-//       index = 0;
-//       continue;
-//     }
-//     let message = ''
-//     if (i % 2 == 0) { // TODO: Make this work for black repertoires
-//       let move_num = (i + 2) / 2;
-//       message += 'You played ' + move_num + ". " + game[i].san + ', whereas your repertoire has ' + (curr_parent.length > 0 ? move_num + ". " +curr_parent[index].san + "  YOU DEVIATED :dissaproval_face:, can't help you" : 'ended');
-//     } else {
-//       message += 'Black played ' + (i + 1) / 2 + ". ..." + game[i].san + ', which was not in your repertoire. You should study it.'
-//     }
-//     document.getElementById('result').innerHTML = message;
-//     break;
-//   }
-//
-// }
-
 let btn = document.querySelector('#show');
-
 
 btn.addEventListener('click', function () {
   let repertoire = window.parent.document.querySelector('#repertoire').value;
@@ -56,8 +26,9 @@ btn.addEventListener('click', function () {
         movesList.appendChild(listItem);
       }
     });
-
-  // const response = '{ "message": "Opponent played d5 deviating from your prep.", "moves": [ "Your move: exd5 (popularity: 55.0%, eval: 1.0)", "", "Opponent has 3 good move(s) in this position", "Opponent has a 0.0% chance to commit a mistake", "Opponent has a 0.0% chance to commit a blunder", "Your move: Nxe5 (popularity: 27.1%, eval: 0.7)", "", "Opponent has 3 good move(s) in this position", "Opponent has a 0.0% chance to commit a mistake", "Opponent has a 0.0% chance to commit a blunder", "Your move: d4 (popularity: 6.2%, eval: 0.3)", "", "Opponent has 3 good move(s) in this position", "Opponent has a 0.0% chance to commit a mistake", "Opponent has a 0.0% chance to commit a blunder" ] }'
+    
+  // Use this for dev instead of actual API calls
+  const response = '{ "message": "Opponent played d5 deviating from your prep.", "moves": [ "Your move: exd5 (popularity: 55.0%, eval: 1.0)", "", "Opponent has 3 good move(s) in this position", "Opponent has a 0.0% chance to commit a mistake", "Opponent has a 0.0% chance to commit a blunder", "Your move: Nxe5 (popularity: 27.1%, eval: 0.7)", "", "Opponent has 3 good move(s) in this position", "Opponent has a 0.0% chance to commit a mistake", "Opponent has a 0.0% chance to commit a blunder", "Your move: d4 (popularity: 6.2%, eval: 0.3)", "", "Opponent has 3 good move(s) in this position", "Opponent has a 0.0% chance to commit a mistake", "Opponent has a 0.0% chance to commit a blunder" ] }'
 
 
 
