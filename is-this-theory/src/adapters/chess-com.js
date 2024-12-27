599e653f7081c7044d010b7fbba98c2bb2e0fb87
Georgios Kousouris <george.kousouris@googlemail.com>
2024-12-27 22:23:19 +0000

    Finding the correct sequence

--- is-this-theory/src/adapters/chess-com.js
+++ is-this-theory/src/adapters/chess-com.js
@@ -10,13 +10,66 @@ class ChessComAdapter {
       (e) => {
         const node = e.target.closest('.node-highlight-content');
         if (node) {
+          const clickedNodeId = node.closest('[data-node]').getAttribute('data-node');
           this.moveMenu.show(node, (moveText) => {
-            console.log('Move clicked:', moveText);
+            // Get all elements with data-node attribute
+            const nodeElements = document.querySelectorAll('[data-node]');
+            const moves = [];
+            const nodeIds = [];
+            
+            // First collect all moves and node IDs up to clicked node
+            for (const element of nodeElements) {
+              const nodeId = element.getAttribute('data-node');
+              const firstSpan = element.querySelector('span');
+              const moveText = firstSpan?.innerText.split(' ')[0];
+              
+              if (moveText) {
+                moves.push(moveText);
+                nodeIds.push(nodeId);
+              }
+
+              // Stop when we reach the clicked node
+              if (nodeId === clickedNodeId) {
+                moves.push(moveText); // Include the clicked node's move
+                nodeIds.push(nodeId); // Include the clicked node's ID
+                break;
+              }
+            }
+
+            this.logMoveData(nodeIds, moves);
           });
         }
       },
       true
     );
+  }
+
+
+  logMoveData(nodeIds, moves) {
+    // Start from the last node ID and work backwards
+    const filteredNodeIds = [];
+    const filteredMoves = [];
+    
+    let currentFirstNum = Infinity;
+    
+    // Process in reverse order
+    for (let i = nodeIds.length - 1; i >= 0; i--) {
+      const nodeId = nodeIds[i];
+      const firstNum = parseInt(nodeId.split('-')[0]);
+      
+      if (firstNum < currentFirstNum) {
+        // Found a smaller first number, just update current
+        currentFirstNum = firstNum;
+      } else if (firstNum === currentFirstNum) {
+        // Same first number, add to filtered list
+        filteredNodeIds.unshift(nodeId);
+        filteredMoves.unshift(moves[i]);
+      }
+      // Ignore if first number is larger
+    }
+    
+    console.log('Filtered Node IDs:', filteredNodeIds);
+    console.log('Corresponding Moves:', filteredMoves);
   }
 
   getBoardElement() {
